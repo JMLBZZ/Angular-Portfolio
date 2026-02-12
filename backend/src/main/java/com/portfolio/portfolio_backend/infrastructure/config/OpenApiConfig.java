6c44a74db1baaea28cd56d761e7705b4c71237d7
NabJam <nabjam@MacBookPro.lan>
2026-02-12 21:35:42 +0100

    OpenAPIConfig : nom, contact, diplôme

--- backend/src/main/java/com/portfolio/portfolio_backend/infrastructure/config/OpenApiConfig.java
+++ backend/src/main/java/com/portfolio/portfolio_backend/infrastructure/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package com.portfolio.portfolio_backend.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,19 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Portfolio API")
                         .version("1.0")
-                        .description("API backend de mon portfolio fullstack"));
+                        .description("API REST pour la gestion de mes projets du portfolio")
+                        .contact(new Contact()
+                                .name("JML")
+                                .email("JML@JML.com")
+                        )
+                        .license(new License()
+                                .name("Master Degree - Directeur de projet en conception et développement de solutions informatiques")
+                        )
+                );
     }
 }
